258645a9c8550c6008be5f6bd670b61feb88e523
SerenityUX <thomas@serenidad.app>
2025-04-23 15:48:33 -0400

    adventure reference

--- site/pages/index.js
+++ site/pages/index.js
@@ -18,14 +18,13 @@ export default function Home() {
     <>
       <Head>
         <title>Neighborhood</title>
-        <meta name="description" content="howdy, this is the neighborhood" />
+        <meta name="description" content="somewhere new" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{display: 'flex', height: "100vh", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
         <video controls style={{width: 300}} src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/90566630dd0e9f5b3cfb53d48cc32a1ad8daefb5_img_3248.mp4"/>
         <br/>
-        <i>"somewhere new"</i>
+        <i>"somewhere new" - adventurer</i>
       </div>
     </>
   );
