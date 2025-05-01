6518ac4eb40a8f62fdaf79ab4473851ad1bc7c76
SerenityUX <thomas@serenidad.app>
2025-05-01 08:39:14 -0400

    adding vercel file

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
+  output: 'standalone',
+  // Ensure images from your domain are allowed
+  images: {
+    domains: ['neighborhood.hackclub.com'],
+  },
 };
 
 export default nextConfig;
